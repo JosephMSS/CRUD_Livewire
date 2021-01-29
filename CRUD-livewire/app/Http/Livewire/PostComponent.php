850fa66482eed3074d7c5fcf19ba36f59adc39da
JosephMMS <josephzx1@hotmail.com>
2021-01-28 19:54:43 -0600

    Formulario de EDITAR con LARAVEL LIVEWIRE

--- CRUD-livewire/app/Http/Livewire/PostComponent.php
+++ CRUD-livewire/app/Http/Livewire/PostComponent.php
@@ -8,32 +8,50 @@ use Livewire\WithPagination;
 
 class PostComponent extends Component
 {
-    public $title,$body;
-    public $view='create';
+    public $title, $body;
+    public $view = 'create';
 
-    use WithPagination;//Eso evita el error mencionado en table.blade
-    
-    protected $paginationTheme = 'bootstrap';//Esto es unicamente en el caso que que  se quierea usar bootstrap
-    
+    use WithPagination; //Eso evita el error mencionado en table.blade
+
+    protected $paginationTheme = 'bootstrap'; //Esto es unicamente en el caso que que  se quierea usar bootstrap
+
     public function render()
     {
-        return view('livewire.post-component',[
-            'posts'=>Post::orderBy('id','desc')->paginate(8)
+        return view('livewire.post-component', [
+            'posts' => Post::orderBy('id', 'desc')->paginate(8)
         ]);
     }
     public function store()
     {
         $this->validate([
-            'title'=>'required',
-            'body'=>'required'
+            'title' => 'required',
+            'body' => 'required'
         ]);
-        Post::create([
-            'title'=>$this->title,
-            'body'=>$this->body,
+
+        $post = Post::create([
+            'title' => $this->title,
+            'body' => $this->body,
         ]);
+
+        $this->edit($post->id);
     }
-     public function destroy($id )
-     {
+    public function edit($id)
+    {
+        $post = Post::find($id);
+
+        $this->title = $post->title;
+        $this->body = $post->body;
+
+        $this->view = 'edit';
+    }
+    public function destroy($id)
+    {
         Post::destroy($id);
-     }
+    }
+    public function default()
+    {
+        $this->title = '';
+        $this->body = '';
+        $this->view = 'create';
+    }
 }
